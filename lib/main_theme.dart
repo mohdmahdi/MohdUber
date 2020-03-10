7ecddcf0857bb98bd4ba6cf1a267d7df1847a260
mohd_ <mohd_em@hotmail.com>
2020-03-11 01:05:33 +0300

    episode 4 - indicators

--- lib/main_theme.dart
+++ lib/main_theme.dart
@@ -1,7 +1,15 @@
-
 import 'package:flutter/material.dart';
 
 ThemeData mainTheme = ThemeData(
   primaryColor: Color(0xffEFBC06),
   primaryColorLight: Color(0xffF9F5EA),
+
+  fontFamily: 'SFPro',
+  textTheme: TextTheme(
+    title: TextStyle(
+        fontWeight: FontWeight.bold,
+        color: Color(0xFF636363),
+        letterSpacing: 1.2 ,height: 1.5),
+    subhead: TextStyle(color: Color(0xFF636363), letterSpacing: 1.2 , height: 1.5),
+  ),
 );
--- lib/util/welcome.dart
+++ lib/util/welcome.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'welcome_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -9,24 +11,41 @@ class WelcomeScreen extends StatefulWidget {
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
-  List<WelcomeModel> welcomes =
-      [
-        WelcomeModel(image: "assets/images/welcome.png" , title: " Welcome1 To mohammed Uber " , body:  ' page 1' ),
-        WelcomeModel(image: "assets/images/welcome1.jpg" , title: " Welcome2 To mohammed Uber " , body: ' page2 ' ),
-        WelcomeModel(image: "assets/images/welcome2.jpg" , title: " Welcome3 To mohammed Uber " , body: ' page3 ' ),
-        WelcomeModel(image: "assets/images/welcome3.jpg" , title: " Welcome4 To mohammed Uber " , body: ' page4 ' ),
-      ];
+  List<WelcomeModel> welcomes = [
+    WelcomeModel(
+        image: "assets/images/welcome.png",
+        title: " Download ,call , go  ",
+        body:
+            ' This is first  long sentences genral can be modified by you at any time with watever words '),
+    WelcomeModel(
+        image: "assets/images/welcome1.jpg",
+        title: " Welcome 2  Uber ",
+        body:
+            ' This is second  long sentences genral can be modified by you at any time with watever words '),
+    WelcomeModel(
+        image: "assets/images/welcome2.jpg",
+        title: " Welcome 3 To  Uber ",
+        body:
+            ' This is third long sentences genral can be modified by you at any time with watever words '),
+    WelcomeModel(
+        image: "assets/images/welcome3.jpg",
+        title: " Welcome 4 To  Uber ",
+        body:
+            ' This is forth long sentences genral can be modified by you at any time with watever words '),
+  ];
 
   PageController _pageController;
-  String nextText ='Next';
-  bool amInLastPage = false ;
+  String nextText = 'Next';
+  bool amInLastPage = false;
+
+  int _currentPosition = 0;
 
   @override
   void initState() {
     super.initState();
 
     _pageController = PageController(
-      initialPage: 1 ,
+      initialPage: 0,
       viewportFraction: 0.75,
     );
   }
@@ -35,82 +54,142 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   void dispose() {
     _pageController.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
-
-    double radius = MediaQuery.of(context).size.width*0.09;
+    double radius = MediaQuery.of(context).size.width * 0.09;
     return Scaffold(
       body: Stack(
         children: <Widget>[
           _slider(context),
-          _bottomNavigation(context , radius),
+          Align(
+              alignment: Alignment.bottomCenter,
+              child: _pageIndicators(context, _currentPosition)),
+          _bottomNavigation(context, radius),
         ],
       ),
     );
   }
-  
-  Widget _slider(BuildContext context){
+
+  Widget _slider(BuildContext context) {
     return Align(
       alignment: Alignment.topCenter,
       child: Padding(
-          padding: EdgeInsets.only(top: 150),
+        padding: EdgeInsets.only(top: 110),
         child: PageView.builder(
-          onPageChanged: (position){
-            if(position == welcomes.length -1 ){
+            onPageChanged: (position) {
               setState(() {
-                nextText = "Get Started";
-                amInLastPage = true;
+                _currentPosition = position;
               });
-            }else{
-              setState(() {
-                nextText = " Next" ;
-                amInLastPage = false ;
-              });
-
-            }
-          },
-          controller: _pageController,
-          itemCount: this.welcomes.length,
-          itemBuilder: (context , position ){
-          return Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              children: <Widget>[
-                Image.asset(welcomes[position].image , fit: BoxFit.cover,),
-                _pageIndicators(context, position),
-                Text(welcomes[0].title),
-                Text(welcomes[0].body),
-              ],
-            ),
-          );
-        }),
-        
+              if (position == welcomes.length - 1) {
+                setState(() {
+                  nextText = "Get Started";
+                  amInLastPage = true;
+                });
+              } else {
+                setState(() {
+                  nextText = " Next";
+                  amInLastPage = false;
+                });
+              }
+            },
+            controller: _pageController,
+            itemCount: this.welcomes.length,
+            itemBuilder: (context, position) {
+              return Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: <Widget>[
+                    Image.asset(
+                      welcomes[position].image,
+                      fit: BoxFit.cover,
+                      height: MediaQuery.of(context).size.height * 0.5,
+                    ),
+                    SizedBox(
+                      height: 48,
+                    ),
+                    Text(
+                      welcomes[position].title,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context)
+                          .textTheme
+                          .title
+                          .copyWith(fontSize: 16),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Text(
+                      welcomes[position].body,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.subhead.copyWith(
+                            fontSize: 14,
+                          ),
+                    ),
+                  ],
+                ),
+              );
+            }),
       ),
     );
-    
   }
 
-  Widget _pageIndicators(BuildContext context , int position) {
-    return Row();
+  Widget _pageIndicators(BuildContext context, int position) {
+    double offset = MediaQuery.of(context).size.height * 0.30;
+    return Transform.translate(
+      offset: Offset(0, -offset),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: _newPageIndicators(),
+      ),
+    );
   }
 
-  Widget _bottomNavigation(BuildContext context , double radius){
+  List<Widget> _newPageIndicators() {
+    List<Widget> widgets = [];
+    for (int i = 0; i < welcomes.length; i++) {
+      widgets.add(_pageIndicator(
+        (_currentPosition == i
+            ? Theme.of(context).primaryColor
+            : Color(0xFFEFDC99)),
+        (i == welcomes.length - 1 ? 0 : 12),
+      ));
+    }
+
+    return widgets;
+  }
+
+  Widget _pageIndicator(Color color, double margin) {
+    return Container(
+      width: 15,
+      height: 5,
+      margin: EdgeInsets.only(right: margin),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.all(Radius.circular(25)),
+      ),
+    );
+  }
+
+  Widget _bottomNavigation(BuildContext context, double radius) {
     return Align(
       alignment: Alignment.bottomCenter,
       child: Container(
         decoration: BoxDecoration(
           color: Theme.of(context).primaryColorLight,
-          borderRadius: BorderRadius.only(topLeft: Radius.circular(radius), topRight: Radius.circular(radius)),
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(radius),
+              topRight: Radius.circular(radius)),
         ),
-
-        height: MediaQuery.of(context).size.height*0.15,
+        height: MediaQuery.of(context).size.height * 0.10,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
             FlatButton(
-              onPressed: () async{
+              onPressed: () async {
                 //TODO:  skip
-                SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+                SharedPreferences sharedPreferences =
+                    await SharedPreferences.getInstance();
                 sharedPreferences.setBool('seen', true);
                 goToHomePage(context);
               },
@@ -126,14 +205,14 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.all(Radius.circular(radius)),
                 ),
-                onPressed: (){
+                onPressed: () {
                   //TODO: go to home page
-                  if(amInLastPage){
+                  if (amInLastPage) {
                     goToHomePage(context);
-                  }else{
-
-                  }
-                  _pageController.nextPage(duration: Duration(milliseconds: 400), curve: Curves.easeInOut);
+                  } else {}
+                  _pageController.nextPage(
+                      duration: Duration(milliseconds: 400),
+                      curve: Curves.easeInOut);
                 },
                 child: Text(nextText),
               ),
@@ -141,12 +220,14 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           ],
         ),
       ),
-
-
     );
   }
 
-  void goToHomePage(BuildContext context){
-    Navigator.of(context).push(MaterialPageRoute(builder: (context) {return HomeScreen();}),);
+  void goToHomePage(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (context) {
+        return HomeScreen();
+      }),
+    );
   }
 }
