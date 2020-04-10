d1c31613dfe0580000129633088dd3b5ed3e5d83
mohd_ <mohd_em@hotmail.com>
2020-04-10 05:56:50 +0300

    end of ep9 ( sign up for both client and driver succssfully )

--- lib/util/home.dart
+++ lib/util/home.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:uberapp/auth/login_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,12 +8,52 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Color _grey = Color(0xFF636363);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(
+          color: _grey
+        ),
+        title: Text(
+          'Home',
+          style: TextStyle(
+              color: _grey,
+              fontWeight: FontWeight.bold,
+              fontSize: 24),
+        ),
+        centerTitle: true,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(),
+            ListTile(
+              onTap: ()async{
+                Navigator.of(context).pop();
+//                Navigator.of(context).pushReplacement(
+//                    MaterialPageRoute(builder: (context) => ProfileScreen()));
+             },
+              trailing: Icon(Icons.person),
+              title: Text('PROFILE'),
+            ),
+            ListTile(
+              onTap: ()async{
+                Navigator.of(context).pop();
+                await FirebaseAuth.instance.signOut();
+                Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) => LoginScreen()));
+              },
+              trailing: Icon(Icons.exit_to_app),
+              title: Text('LOGOUT'),
+            )
+          ],
+        ),
+      ),
       body: Center(
-        child: Text('Home Screen'),
-        
+          child: Text('HOME SCREEN'),
       ),
     );
   }
